875a7cd0ada54fc75488fe0d8304d9b085485672
Muhammed Ali Sit <masit@outlook.com>
2019-03-06 16:13:02 -0600

    two sample cli runs with php and ruby

---
+++ Sample/sample.rb
@@ -0,0 +1,11 @@
+inputfile = '90.bin'
+inputtype = 'bin'
+outputfile = 'ws.kml'
+outputtype = 'kml'
+x = 4777
+y = 897
+
+# This implementation assumes that a compiled executable named as "watershed"
+# exists in the same directory as this script.
+cmd = "./watershed -i #{inputfile} -o #{outputfile} -t #{inputtype} -z #{outputtype} -x #{x} -y #{y}"
+system cmd
